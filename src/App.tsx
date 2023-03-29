d0e4175447372dca788564ad764e7eaafc80752f
MAlngaawy <m.alngaawy@gmail.com>
2023-03-29 11:00:16 +0200

    use tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Counter from './main/Components/Counter'
-import CounterContextProvider from './main/context/CounterContext'
+import Counter from "./main/Components/Counter";
+import CounterContextProvider from "./main/context/CounterContext";
 
 function App() {
-
   return (
     <CounterContextProvider>
       <Counter />
     </CounterContextProvider>
-    )
+  );
 }
 
-export default App
+export default App;
--- src/main/Components/Counter.tsx
+++ src/main/Components/Counter.tsx
@@ -1,21 +1,19 @@
-import React, { useContext } from 'react'
-import { CounterCotext } from '../context/CounterContext'
+import React, { useContext } from "react";
+import { CounterCotext } from "../context/CounterContext";
 
-type Props = {}
+type Props = {};
 
 const Counter = (props: Props) => {
-  const [counter, setCounter] = useContext(CounterCotext)
+  const [counter, setCounter] = useContext(CounterCotext);
   return (
-    <>
-    <div>Counter Is {counter}</div>
-    <button onClick={() => setCounter((v: number) => v+1)}>
-      Click To Add One
-    </button>
-    <button onClick={() => setCounter(0)}>
-      Reset
-    </button>
-    </>
-  )
-}
+    <div className="flex flex-col w-screen place-items-center gap-3">
+      <div>Counter Is {counter}</div>
+      <button onClick={() => setCounter((v: number) => v + 1)}>
+        Click To Add One
+      </button>
+      <button onClick={() => setCounter(0)}>Reset</button>
+    </div>
+  );
+};
 
-export default Counter
+export default Counter;
